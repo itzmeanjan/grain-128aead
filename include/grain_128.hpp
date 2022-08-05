68ec03ed199f3b1a310dcc571aa23ab3b1a7b147
Anjan Roy <hello@itzmeanjan.in>
2022-08-05 19:34:12 +0400

    implement boolean function `h(x)`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/grain_128.hpp
+++ include/grain_128.hpp
@@ -6,6 +6,22 @@
 // Grain-128 Authenticated Encryption with Associated Data
 namespace grain_128 {
 
+// Grain-128 AEAD state, which has two main parts
+//
+// i)  Preoutput Generator
+//      a) 128 -bit LFSR
+//      b) 128 -bit NFSR
+// ii) Authentication Generator
+//      a) 64 -bit Accumulator
+//      b) 64 -bit Shift Register
+struct state_t
+{
+  uint8_t lfsr[16]; // 128 -bit linear feedback shift register
+  uint8_t nfsr[16]; // 128 -bit non-linear feedback shift register
+  uint8_t acc[8];   // 64 -bit accumulator
+  uint8_t sreg[8];  // 64 -bit shift register
+};
+
 // Given a bit index in a bit array, this function computes byte index ( =
 // selected byte ) & bit to pick up from the selected byte, when the bit
 // array is represented as a byte array
@@ -38,6 +54,41 @@ select_bit(
   const uint8_t bit = (byte >> idx.second) & one;
 
   return bit;
+}
+
+// Boolean function `h(x)`, which takes 9 state variable bits & produces single
+// bit, using formula
+//
+// h(x) = x0x1 + x2x3 + x4x5 + x6x7 + x0x4x8
+//
+// 2 of these input bits are from NFSR, while remaining 7 of them are from LFSR.
+//
+// Bits correspond to (x0, x1, ...x7, x8) -> (NFSR12, LFSR8, LFSR13, LFSR20,
+// NFSR95, LFSR42, LFSR60, LFSR79, LFSR94)
+//
+// See definition of `h(x)` function in page 7 of Grain-128 AEAD specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/grain-128aead-spec-final.pdf
+inline static void
+h(const state_t* const st)
+{
+  const uint8_t x0 = select_bit(st->nfsr, compute_index(12));
+  const uint8_t x1 = select_bit(st->lfsr, compute_index(8));
+  const uint8_t x2 = select_bit(st->lfsr, compute_index(13));
+  const uint8_t x3 = select_bit(st->lfsr, compute_index(20));
+  const uint8_t x4 = select_bit(st->nfsr, compute_index(95));
+  const uint8_t x5 = select_bit(st->lfsr, compute_index(42));
+  const uint8_t x6 = select_bit(st->lfsr, compute_index(60));
+  const uint8_t x7 = select_bit(st->lfsr, compute_index(79));
+  const uint8_t x8 = select_bit(st->lfsr, compute_index(94));
+
+  const uint8_t x0x1 = x0 & x1;
+  const uint8_t x2x3 = x2 & x3;
+  const uint8_t x4x5 = x4 & x5;
+  const uint8_t x6x7 = x6 & x7;
+  const uint8_t x0x4x8 = x0 & x4 & x8;
+
+  const uint8_t hx = x0x1 ^ x2x3 ^ x4x5 ^ x6x7 ^ x0x4x8;
+  return hx;
 }
 
 }
